16053cde3c940c54c0e36cdeecbf262ae09a9d68
Iarlen Aquiles <iarlensilva775@gmail.com>
2022-01-29 13:37:33 -0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
-import DraftLog from "draftlog";
-import chalk from "chalk";
-import chalkTable from "chalk-table";
-import readline from "readline";
 import database from "./../database.json" assert { type: "json" };
-import Person from "./person.js";
+import TerminalController from "./terminalController.js";
 
-DraftLog(console).addLineListener(process.stdin);
+const DEFAULT_LANG = "pt-BR";
 
-const options = {
-  leftPad: 2,
-  columns: [
-    { field: "id", name: chalk.cyan("ID") },
-    { field: "vehicles", name: chalk.magenta("vehicles") },
-    { field: "kmTraveled", name: chalk.cyan("kmTraveled") },
-    { field: "from", name: chalk.cyan("from") },
-    { field: "to", name: chalk.cyan("to") },
-  ],
-};
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANG);
 
-const table = chalkTable(options, database.map(item => new Person(item).formatted()));
-const print = console.draft(table);
-
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-terminal.question("Qual é seu nome?", (msg) => {
-  console.log("msg", msg.toString());
-});
